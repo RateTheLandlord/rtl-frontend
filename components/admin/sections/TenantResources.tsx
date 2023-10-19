a426cb8c6bf57c603b1048ae0136d4f94de93f78
kellenwiltshire <kellenwiltshire@outlook.com>
2023-10-19 16:49:00 -0400

    minor fix

--- components/admin/sections/TenantResources.tsx
+++ components/admin/sections/TenantResources.tsx
@@ -82,7 +82,6 @@ const TenantResources = () => {
 			})
 			.finally(() => setLoading(false))
 	}
-	if (!data.resources.length) return <div>No Resources to Display</div>
 	return (
 		<div className='container flex w-full flex-col justify-center'>
 			{removeAlertOpen ? (
@@ -146,90 +145,93 @@ const TenantResources = () => {
 					Add New Resource
 				</Button>
 			</div>
-			<ul
-				role='list'
-				className='mt-2 divide-y divide-gray-100 border-t border-gray-100'
-			>
-				{data.resources.map((resource) => (
-					<li
-						key={resource.id}
-						className='flex items-center justify-between gap-x-6 py-3'
-					>
-						<div className='min-w-0'>
-							<div className='flex items-center justify-start gap-x-3'>
-								<p className='text-sm font-semibold leading-6 text-gray-900'>
-									{resource.name}
-								</p>
+			{data.resources.length && (
+				<ul
+					role='list'
+					className='mt-2 divide-y divide-gray-100 border-t border-gray-100'
+				>
+					{data.resources.map((resource) => (
+						<li
+							key={resource.id}
+							className='flex items-center justify-between gap-x-6 py-3'
+						>
+							<div className='min-w-0'>
+								<div className='flex items-center justify-start gap-x-3'>
+									<p className='text-sm font-semibold leading-6 text-gray-900'>
+										{resource.name}
+									</p>
 
-								<p className='text-xs'>{resource.phone_number}</p>
+									<p className='text-xs'>{resource.phone_number}</p>
+								</div>
+								<div className='mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500'>
+									<p className='whitespace-nowrap'>
+										<p>{`${resource.address}, ${resource.city}, ${resource.state}, ${resource.country_code}`}</p>
+									</p>
+									<svg viewBox='0 0 2 2' className='h-0.5 w-0.5 fill-current'>
+										<circle cx={1} cy={1} r={1} />
+									</svg>
+									<p className='truncate'>{resource.description}</p>
+								</div>
 							</div>
-							<div className='mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500'>
-								<p className='whitespace-nowrap'>
-									<p>{`${resource.address}, ${resource.city}, ${resource.state}, ${resource.country_code}`}</p>
-								</p>
-								<svg viewBox='0 0 2 2' className='h-0.5 w-0.5 fill-current'>
-									<circle cx={1} cy={1} r={1} />
-								</svg>
-								<p className='truncate'>{resource.description}</p>
-							</div>
-						</div>
-						<div className='flex flex-none items-center gap-x-4'>
-							<Menu as='div' className='relative flex-none'>
-								<Menu.Button className='-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900'>
-									<span className='sr-only'>Open options</span>
-									<MenuAlt3Icon className='h-5 w-5' aria-hidden='true' />
-								</Menu.Button>
-								<Transition
-									as={Fragment}
-									enter='transition ease-out duration-100'
-									enterFrom='transform opacity-0 scale-95'
-									enterTo='transform opacity-100 scale-100'
-									leave='transition ease-in duration-75'
-									leaveFrom='transform opacity-100 scale-100'
-									leaveTo='transform opacity-0 scale-95'
-								>
-									<Menu.Items className='absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none'>
-										<Menu.Item>
-											{({ active }) => (
-												<button
-													onClick={() => {
-														setEditResourceOpen(true)
-														setSelectedResource(resource)
-													}}
-													className={classNames(
-														active ? 'bg-gray-50' : '',
-														'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900',
-													)}
-												>
-													Edit<span className='sr-only'>, {resource.name}</span>
-												</button>
-											)}
-										</Menu.Item>
+							<div className='flex flex-none items-center gap-x-4'>
+								<Menu as='div' className='relative flex-none'>
+									<Menu.Button className='-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900'>
+										<span className='sr-only'>Open options</span>
+										<MenuAlt3Icon className='h-5 w-5' aria-hidden='true' />
+									</Menu.Button>
+									<Transition
+										as={Fragment}
+										enter='transition ease-out duration-100'
+										enterFrom='transform opacity-0 scale-95'
+										enterTo='transform opacity-100 scale-100'
+										leave='transition ease-in duration-75'
+										leaveFrom='transform opacity-100 scale-100'
+										leaveTo='transform opacity-0 scale-95'
+									>
+										<Menu.Items className='absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none'>
+											<Menu.Item>
+												{({ active }) => (
+													<button
+														onClick={() => {
+															setEditResourceOpen(true)
+															setSelectedResource(resource)
+														}}
+														className={classNames(
+															active ? 'bg-gray-50' : '',
+															'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900',
+														)}
+													>
+														Edit
+														<span className='sr-only'>, {resource.name}</span>
+													</button>
+												)}
+											</Menu.Item>
 
-										<Menu.Item>
-											{({ active }) => (
-												<button
-													onClick={() => {
-														setDeleteResourceOpen(true)
-														setSelectedResource(resource)
-													}}
-													className={classNames(
-														active ? 'bg-gray-50' : '',
-														'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900',
-													)}
-												>
-													Delete
-													<span className='sr-only'>, {resource.name}</span>
-												</button>
-											)}
-										</Menu.Item>
-									</Menu.Items>
-								</Transition>
-							</Menu>
-						</div>
-					</li>
-				))}
-			</ul>
+											<Menu.Item>
+												{({ active }) => (
+													<button
+														onClick={() => {
+															setDeleteResourceOpen(true)
+															setSelectedResource(resource)
+														}}
+														className={classNames(
+															active ? 'bg-gray-50' : '',
+															'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900',
+														)}
+													>
+														Delete
+														<span className='sr-only'>, {resource.name}</span>
+													</button>
+												)}
+											</Menu.Item>
+										</Menu.Items>
+									</Transition>
+								</Menu>
+							</div>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	)
 }
