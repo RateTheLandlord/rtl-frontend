e37813d0bb16386568aff62ff0635f1b3af9a261
stephenmartin2951 <68044697+stephenmartin2951@users.noreply.github.com>
2023-10-22 17:20:24 -0500

    #3 Added ability to sort by rating (#38)

--- util/helpers/filter-options.ts
+++ util/helpers/filter-options.ts
@@ -3,4 +3,6 @@ export const sortOptions = [
 	{id: 2, name: 'Name Z-A', value: 'za'},
 	{id: 3, name: 'Newest', value: 'new'},
 	{id: 4, name: 'Oldest', value: 'old'},
+	{id: 4, name: 'Rating Highest', value: 'high'},
+	{id: 4, name: 'Rating Lowest', value: 'low'},
 ]
