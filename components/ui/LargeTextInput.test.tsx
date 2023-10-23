5b59d5c621b02a265092c5b181490c9eea1e5051
kellenwiltshire <kellenwiltshire@outlook.com>
2023-10-23 19:11:44 -0400

    fixing tests

--- components/ui/LargeTextInput.test.tsx
+++ components/ui/LargeTextInput.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import LargeTextInput from './LargeTextInput'
 
