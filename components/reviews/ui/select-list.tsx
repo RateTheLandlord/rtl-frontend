034b26357c0f6b821a942e43339e1805f7e10356
Kellen Wiltshire <Kellenwiltshire@outlook.com>
2023-10-24 07:58:27 -0400

    Slight Polish Changes (#41)

--- components/reviews/ui/select-list.tsx
+++ components/reviews/ui/select-list.tsx
@@ -18,7 +18,7 @@ export default function SelectList({
 }: ComponentProps) {
 	return (
 		<Listbox value={state} onChange={setState}>
-			<div className='relative px-2'>
+			<div className='px-2'>
 				<Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
 					<span className='block w-full truncate'>{state?.name || name}</span>
 					<span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
