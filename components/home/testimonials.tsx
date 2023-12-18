8275a4ce82b48fa43be44879d7d20f59145076aa
kellenwiltshire <kellenwiltshire@outlook.com>
2023-12-18 08:17:04 -0500

    Update tests

--- components/home/testimonials.tsx
+++ components/home/testimonials.tsx
@@ -39,27 +39,28 @@ const testimonials = [
 
 export default function Testimonials() {
 	return (
-		<div>
-			<div className='mx-auto max-w-7xl px-6 lg:px-8'>
-				<div className='mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none'>
-					<div className='-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3'>
-						{testimonials.map((testimonial, idk) => (
-							<div key={idk} className='pt-8 sm:inline-block sm:w-full sm:px-4'>
-								<figure className='rounded-2xl bg-gray-50 p-8 text-sm leading-6'>
-									<blockquote className='text-gray-900'>
-										<p>{`“${testimonial.body}”`}</p>
-									</blockquote>
-									<figcaption className='mt-6 flex items-center gap-x-4'>
-										<div>
-											<div className='font-semibold text-gray-900'>
-												{testimonial.author.name}
-											</div>
+		<div
+			data-testid='home-testimonial'
+			className='mx-auto max-w-7xl px-6 lg:px-8'
+		>
+			<div className='mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none'>
+				<div className='-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3'>
+					{testimonials.map((testimonial, idk) => (
+						<div key={idk} className='pt-8 sm:inline-block sm:w-full sm:px-4'>
+							<figure className='rounded-2xl bg-gray-50 p-8 text-sm leading-6'>
+								<blockquote className='text-gray-900'>
+									<p>{`“${testimonial.body}”`}</p>
+								</blockquote>
+								<figcaption className='mt-6 flex items-center gap-x-4'>
+									<div>
+										<div className='font-semibold text-gray-900'>
+											{testimonial.author.name}
 										</div>
-									</figcaption>
-								</figure>
-							</div>
-						))}
-					</div>
+									</div>
+								</figcaption>
+							</figure>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
