9ee9558347773de180fc52783327f0aa60073602
kellenwiltshire <kellenwiltshire@outlook.com>
2023-12-20 07:52:41 -0500

    Polish homepage

--- components/home/testimonials.tsx
+++ components/home/testimonials.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 const testimonials = [
 	{
 		body: 'sounds like a great initiative, count me in!',
@@ -35,25 +37,47 @@ const testimonials = [
 			name: 'Instagram User',
 		},
 	},
+	{
+		body: `So I’m a landlord and love this page. If you\'re a landlord and have a problem with this site, you are literally the problem. It\'s incredibly fucking easy to be a good landlord.`,
+		author: {
+			name: 'Instagram Landlord',
+		},
+	},
 ]
 
 export default function Testimonials() {
+	const [currTestimonial, setCurrTestimonial] = useState(0)
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			const num = Math.floor(Math.random() * testimonials.length)
+			setCurrTestimonial(num)
+		}, 5000)
+
+		return () => clearInterval(timer)
+	}, [currTestimonial])
 	return (
 		<div
 			data-testid='home-testimonial'
 			className='mx-auto max-w-7xl px-6 lg:px-8'
 		>
-			<div className='mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none'>
+			<div className='mx-auto my-8 flow-root max-w-2xl lg:mx-0 lg:max-w-none'>
 				<div className='-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3'>
-					{testimonials.map((testimonial, idk) => (
-						<div key={idk} className='pt-8 sm:inline-block sm:w-full sm:px-4'>
-							<figure className='rounded-2xl bg-gray-50 p-8 text-sm leading-6'>
-								<blockquote className='text-gray-900'>
+					{testimonials.map((testimonial, idx) => (
+						<div key={idx} className='pt-8 sm:inline-block sm:w-full sm:px-4'>
+							<figure
+								className={`rounded-2xl p-8 text-sm leading-6 transition-all duration-500 ease-in-out ${
+									idx === currTestimonial
+										? 'scale-110 bg-teal-600 text-white'
+										: 'scale-100 bg-gray-50 text-gray-900'
+								}`}
+							>
+								<blockquote>
 									<p>{`“${testimonial.body}”`}</p>
 								</blockquote>
 								<figcaption className='mt-6 flex items-center gap-x-4'>
 									<div>
-										<div className='font-semibold text-gray-900'>
+										<div className='font-semibold'>
 											{testimonial.author.name}
 										</div>
 									</div>
