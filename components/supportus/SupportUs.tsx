9857119f7d3cd2cf2ea8f2734727e5d24c5596d2
kellenwiltshire <kellenwiltshire@outlook.com>
2023-12-28 10:28:33 -0500

    update spacing

--- components/supportus/SupportUs.tsx
+++ components/supportus/SupportUs.tsx
@@ -46,7 +46,7 @@ export default function Support() {
 						costs—from server maintenance to development resources.
 					</p>
 				</div>
-				<div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'>
+				<div className='mx-auto my-16 max-w-2xl sm:my-20 lg:my-24 lg:max-w-4xl'>
 					<dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16'>
 						{features.map((feature) => (
 							<div key={feature.name} className='relative pl-16'>
