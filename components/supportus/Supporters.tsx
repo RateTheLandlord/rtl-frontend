4f755ed623fddbc6d41e41cb3790682718490bb0
kellenwiltshire <kellenwiltshire@outlook.com>
2024-02-07 08:10:31 -0500

    Add Patreon Supporters Section

---
+++ components/supportus/Supporters.tsx
@@ -0,0 +1,43 @@
+import { MemberData, TierData } from '@/util/helpers/patreon'
+
+interface IProps {
+	members: Array<MemberData>
+	tiers: Array<TierData>
+}
+
+export default function Supporters({ members, tiers }: IProps) {
+	const getTierName = (id: number) => {
+		const found = tiers.find((tier) => tier.id === id)
+		return found?.name
+	}
+	return (
+		<div>
+			<h3 className='text-center text-4xl font-extrabold'>
+				Our Generous Supporters
+			</h3>
+			<div className='mx-auto mt-3 max-w-7xl px-6 lg:px-8'>
+				<div className='grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-4'>
+					{members
+						.map((member) => {
+							const tierName = getTierName(member.id)
+							if (tierName === 'Free') {
+								return null
+							}
+							return (
+								<div
+									key={member.name}
+									className='bg-gray-400/5 p-8 text-center sm:p-10'
+								>
+									<div>
+										<h6 className='text-xl font-bold'>{member.name}</h6>
+										<p>{tierName}</p>
+									</div>
+								</div>
+							)
+						})
+						.reverse()}
+				</div>
+			</div>
+		</div>
+	)
+}
--- pages/support-us.tsx
+++ pages/support-us.tsx
@@ -2,8 +2,10 @@ import Support from '@/components/supportus/SupportUs'
 import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { getMemberData, getTierData } from '@/util/helpers/patreon'
+import Supporters from '@/components/supportus/Supporters'
 
-export default function SupportUs(): JSX.Element {
+export default function SupportUs({ members }): JSX.Element {
 	const title = 'Support Us | Rate The Landlord'
 	const desc =
 		'Share information with tenants like you and rate your landlord. We are a community platform that elevates tenant voices to promote landlord accountability. Find Landlord Reviews and Resources.'
@@ -12,6 +14,9 @@ export default function SupportUs(): JSX.Element {
 	const pageURL = pathName === '/' ? siteURL : siteURL + pathName
 	const twitterHandle = '@r8thelandlord'
 	const siteName = 'RateTheLandlord.org'
+
+	const memberData = getMemberData(members)
+	const tierData = getTierData(members)
 
 	return (
 		<div>
@@ -47,6 +52,54 @@ export default function SupportUs(): JSX.Element {
 				]}
 			/>
 			<Support />
+			<Supporters members={memberData} tiers={tierData} />
 		</div>
 	)
+}
+
+//Page is statically generated at build time and then revalidated at a minimum of every day based on when the page is accessed
+export async function getStaticProps() {
+	const accessToken = process.env.PATREON_ACCESS_TOKEN as string
+
+	const campaignId = await fetch(
+		'https://www.patreon.com/api/oauth2/api/current_user/campaigns',
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
+		},
+	)
+		.then((res) => {
+			return res.json()
+		})
+		.then((data) => {
+			return data.data[0].id
+		})
+		.catch((err) => console.log('Error: ', err))
+
+	console.log(campaignId)
+	const members = await fetch(
+		`https://www.patreon.com/api/oauth2/v2/campaigns/${campaignId}/members?include=currently_entitled_tiers&fields%5Bmember%5D=full_name&fields%5Btier%5D=amount_cents,created_at,discord_role_ids,edited_at,patron_count,title,url`,
+		{
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${accessToken}`,
+			},
+		},
+	)
+		.then((res) => {
+			return res.json()
+		})
+		.then((data) => {
+			return data
+		})
+		.catch((err) => console.log('Error: ', err))
+
+	return {
+		props: {
+			members: members,
+		},
+		revalidate: 86400,
+	}
 }
