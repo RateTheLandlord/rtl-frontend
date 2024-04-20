6ac81303640f6ef3fe1b75a6c7b5da656bccf79f
Kellen Wiltshire <Kellenwiltshire@outlook.com>
2024-04-20 18:26:53 -0400

    Posthog addition (#111)
    * added posthog
    
    * update privacy policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,20 +7,35 @@ import { store } from '@/redux/store'
 import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
 const CAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY as string
+
+if (typeof window !== 'undefined') {
+	// checks that we are client-side
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+		loaded: (posthog) => {
+			if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+		},
+	})
+}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	const { user } = pageProps
 	return (
-		<UserProvider user={user}>
-			<Provider store={store}>
-				<ReCaptchaProvider reCaptchaKey={CAPTCHA_SITE_KEY} useEnterprise>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</ReCaptchaProvider>
-			</Provider>
-		</UserProvider>
+		<PostHogProvider client={posthog}>
+			<UserProvider user={user}>
+				<Provider store={store}>
+					<ReCaptchaProvider reCaptchaKey={CAPTCHA_SITE_KEY} useEnterprise>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</ReCaptchaProvider>
+				</Provider>
+			</UserProvider>
+		</PostHogProvider>
 	)
 }
 
