7dc7e02b18fb59506836881019b7319f54057483
stephenmartin2951 <stephenmartin2951@pm.me>
2024-07-25 16:58:53 -0500

    #148 formatting changes

--- components/landlord/OtherLandlord.tsx
+++ components/landlord/OtherLandlord.tsx
@@ -24,7 +24,7 @@ const OtherLandlordInfo = ({ landlord }: IProps) => {
 			<h3 className='mt-4 text-lg text-gray-900'>
 				View Other Landlords in {landlords[0]?.topcity}:
 			</h3>
-			<div className='grid grid-cols-2 gris-rows-5 gap-2 lg:grid-cols-5 lg:grid-rows-2'>
+			<div className='gris-rows-5 grid grid-cols-2 gap-2 lg:grid-cols-5 lg:grid-rows-2'>
 				{landlords.map((otherLandlord, index) => {
 					return (
 						<Link
@@ -32,19 +32,19 @@ const OtherLandlordInfo = ({ landlord }: IProps) => {
 							href={`/landlord/${encodeURIComponent(otherLandlord.name)}`}
 							className='flex items-center justify-center rounded-md border bg-teal-600/5 p-2 hover:underline'
 						>
-						<div className='flex flex-col items-center justify-center'>
-							<div
-								className='col mb-4 flex w-full cursor-pointer flex-col break-words text-lg lg:mb-2 lg:items-center'
-								data-umami-event='Reviews / Landlord Link'
-							>
-								<h6 className='text-center'>{otherLandlord.name}</h6>
-								<p className='text-center text-sm'>
-									Read {otherLandlord.reviewcount} review(s)
-								</p>
+							<div className='flex flex-col items-center justify-center'>
+								<div
+									className='col mb-4 flex w-full cursor-pointer flex-col break-words text-lg lg:mb-2 lg:items-center'
+									data-umami-event='Reviews / Landlord Link'
+								>
+									<h6 className='text-center'>{otherLandlord.name}</h6>
+									<p className='text-center text-sm'>
+										Read {otherLandlord.reviewcount} review(s)
+									</p>
+								</div>
+								<RatingStars value={Math.floor(otherLandlord.avgrating)} />
 							</div>
-							<RatingStars value={Math.floor(otherLandlord.avgrating)} />
-						</div>
-					</Link>
+						</Link>
 					)
 				})}
 			</div>
