0fa6cd0b287b44b667d12987a42fd33450ff1f3e
stephenmartin2951 <stephenmartin2951@pm.me>
2024-07-25 16:58:05 -0500

    #148 other landlord formatting

--- components/landlord/OtherLandlord.tsx
+++ components/landlord/OtherLandlord.tsx
@@ -15,14 +15,6 @@ const OtherLandlordInfo = ({ landlord }: IProps) => {
 		fetcher,
 	)
 
-	const myComponent = {
-		width: '225px',
-		height: '225px',
-		overflow: 'scroll',
-		alignItems: 'center',
-		justifyContent: 'center'
-	};
-
 	if (!landlords) return <Spinner />
 
 	if (error || !landlords.length) return null
@@ -32,28 +24,27 @@ const OtherLandlordInfo = ({ landlord }: IProps) => {
 			<h3 className='mt-4 text-lg text-gray-900'>
 				View Other Landlords in {landlords[0]?.topcity}:
 			</h3>
-			<div className='flex flex-wrap items-center gap-3 xl:col-span-1'>
+			<div className='grid grid-cols-2 gris-rows-5 gap-2 lg:grid-cols-5 lg:grid-rows-2'>
 				{landlords.map((otherLandlord, index) => {
 					return (
-						<div
+						<Link
 							key={index}
-							className='flex flex-row flex-wrap items-center justify-between rounded-md border bg-teal-600/5 p-2 lg:flex-col'
-							style={myComponent}
+							href={`/landlord/${encodeURIComponent(otherLandlord.name)}`}
+							className='flex items-center justify-center rounded-md border bg-teal-600/5 p-2 hover:underline'
 						>
-							<div className='flex flex-col items-center'>
-								<Link
-									href={`/landlord/${encodeURIComponent(otherLandlord.name)}`}
-									className='col mb-4 flex w-full cursor-pointer flex-col break-words text-lg  hover:underline lg:mb-2 lg:items-center'
-									data-umami-event='Reviews / Landlord Link'
-								>
-									<h6 className='text-center'>{otherLandlord.name}</h6>
-									<p className='text-center text-sm'>
-										Read {otherLandlord.reviewcount} review(s)
-									</p>
-								</Link>
-								<RatingStars value={Math.floor(otherLandlord.avgrating)} />
+						<div className='flex flex-col items-center justify-center'>
+							<div
+								className='col mb-4 flex w-full cursor-pointer flex-col break-words text-lg lg:mb-2 lg:items-center'
+								data-umami-event='Reviews / Landlord Link'
+							>
+								<h6 className='text-center'>{otherLandlord.name}</h6>
+								<p className='text-center text-sm'>
+									Read {otherLandlord.reviewcount} review(s)
+								</p>
 							</div>
+							<RatingStars value={Math.floor(otherLandlord.avgrating)} />
 						</div>
+					</Link>
 					)
 				})}
 			</div>
