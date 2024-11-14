d43d718778542367cc0cd8822877e8ca981e530d
stephenmartin2951 <stephenmartin2951@pm.me>
2024-11-14 15:47:26 -0600

    Map defaults to filtered location TODO: Why does Chicago show twice in dynamic city options

--- components/reviews/review.tsx
+++ components/reviews/review.tsx
@@ -309,7 +309,10 @@ const Review = ({
 									</div>
 								</TabPanel>
 								<TabPanel>
-									<MapComponent />
+									<MapComponent
+										countryFilter={countryFilter}
+										stateFilter={stateFilter}
+									/>
 									<p className='mt-6 text-xs leading-7 text-gray-600'>
 										This map component is currently in Beta. It is provided “as
 										is” and may contain bugs, inaccuracies, or incomplete
