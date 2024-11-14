232a5a88c2e8bec67dd725bc4a87acb71ba91a61
stephenmartin2951 <stephenmartin2951@pm.me>
2024-11-14 14:51:05 -0600

    lint

--- components/reviews/review.tsx
+++ components/reviews/review.tsx
@@ -203,126 +203,128 @@ const Review = ({
 					/>
 				</>
 			) : null}
-			{ reviewsLoading ? (
+			{reviewsLoading ? (
 				<Spinner />
 			) : (
-			<div className='w-full'>
-				<AdsComponent slot='2009320000' />
-				<div>
-					<div className='mx-auto max-w-7xl border-b-gray-200 px-4 py-16 sm:px-6 lg:border-b lg:px-8'>
-						<div>
-							<h1 className='text-3xl   text-gray-900'>{t('reviews.title')}</h1>
-							<p className='mt-4 max-w-xl text-sm text-gray-700'>
-								{t('reviews.body')}
-							</p>
+				<div className='w-full'>
+					<AdsComponent slot='2009320000' />
+					<div>
+						<div className='mx-auto max-w-7xl border-b-gray-200 px-4 py-16 sm:px-6 lg:border-b lg:px-8'>
+							<div>
+								<h1 className='text-3xl   text-gray-900'>
+									{t('reviews.title')}
+								</h1>
+								<p className='mt-4 max-w-xl text-sm text-gray-700'>
+									{t('reviews.body')}
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className='flex w-full justify-end px-4 lg:hidden'>
-					<ButtonLight onClick={() => setMobileFiltersOpen(true)}>
-						{t('reviews.filters')}
-					</ButtonLight>
-				</div>
-				<div className='mx-auto max-w-2xl lg:max-w-7xl'>
-					<TabGroup
-						selectedIndex={selectedIndex}
-						onChange={setSelectedIndex}
-						as='div'
-						className='w-full'
-					>
-						<TabList className='flex w-full justify-center gap-4 border-b p-3'>
-							<Tab className='whitespace-nowrap border-b-2 border-transparent px-1 pb-2 text-3xl font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:outline-none data-[selected]:border-indigo-500 data-[selected]:text-indigo-600'>
-								Reviews
-							</Tab>
-							<Tab className='whitespace-nowrap border-b-2 border-transparent px-1 pb-2 text-3xl font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:outline-none data-[selected]:border-indigo-500 data-[selected]:text-indigo-600'>
-								<div className='flex flex-row gap-1'>
-									<p>Map</p>
-									<div className='flex h-full flex-col justify-start'>
-										<span className='inline-flex items-center rounded-md bg-teal-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-teal-500/10'>
-											Beta
-										</span>
-									</div>
-								</div>
-							</Tab>
-						</TabList>
-						<TabPanels>
-							<TabPanel>
-								<div className='flex lg:flex-row lg:gap-2 lg:divide-x lg:divide-gray-200'>
-									<MobileReviewFilters
-										mobileFiltersOpen={mobileFiltersOpen}
-										setMobileFiltersOpen={setMobileFiltersOpen}
-										countryFilter={countryFilter}
-										stateFilter={stateFilter}
-										cityFilter={cityFilter}
-										zipFilter={zipFilter}
-										dynamicCityOptions={dynamicCityOptions}
-										dynamicStateOptions={dynamicStateOptions}
-										zipOptions={dynamicZipOptions}
-										dynamicZipOptions={dynamicZipOptions}
-										updateParams={updateParams}
-										dispatch={dispatch}
-										fetchDynamicFilterOptions={fetchDynamicFilterOptions}
-										query={query}
-									/>
-									<ReviewFilters
-										selectedSort={selectedSort}
-										setSelectedSort={setSelectedSort}
-										sortOptions={sortOptions}
-										countryFilter={countryFilter}
-										stateFilter={stateFilter}
-										cityFilter={cityFilter}
-										zipFilter={zipFilter}
-										dynamicCityOptions={dynamicCityOptions}
-										dynamicStateOptions={dynamicStateOptions}
-										zipOptions={dynamicZipOptions}
-										dynamicZipOptions={dynamicZipOptions}
-										updateParams={updateParams}
-										loading={isLoading}
-										dispatch={dispatch}
-										fetchDynamicFilterOptions={fetchDynamicFilterOptions}
-										query={query}
-									/>
-									{!reviews.length ? (
-										<div className='mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center p-6'>
-											<h1 className='mt-4 text-3xl   text-gray-900 sm:text-5xl'>
-												No results found
-											</h1>
-											<p className='mt-6 text-base leading-7 text-gray-600'>
-												Sorry, we couldn't find any results for those filters.
-											</p>
+					<div className='flex w-full justify-end px-4 lg:hidden'>
+						<ButtonLight onClick={() => setMobileFiltersOpen(true)}>
+							{t('reviews.filters')}
+						</ButtonLight>
+					</div>
+					<div className='mx-auto max-w-2xl lg:max-w-7xl'>
+						<TabGroup
+							selectedIndex={selectedIndex}
+							onChange={setSelectedIndex}
+							as='div'
+							className='w-full'
+						>
+							<TabList className='flex w-full justify-center gap-4 border-b p-3'>
+								<Tab className='whitespace-nowrap border-b-2 border-transparent px-1 pb-2 text-3xl font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:outline-none data-[selected]:border-indigo-500 data-[selected]:text-indigo-600'>
+									Reviews
+								</Tab>
+								<Tab className='whitespace-nowrap border-b-2 border-transparent px-1 pb-2 text-3xl font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:outline-none data-[selected]:border-indigo-500 data-[selected]:text-indigo-600'>
+									<div className='flex flex-row gap-1'>
+										<p>Map</p>
+										<div className='flex h-full flex-col justify-start'>
+											<span className='inline-flex items-center rounded-md bg-teal-50 px-1.5 py-0.5 text-xs font-medium text-gray-600 ring-1 ring-inset ring-teal-500/10'>
+												Beta
+											</span>
 										</div>
-									) : (
-										<InfiniteScroll
-											data={reviews}
-											setReportOpen={setReportOpen}
-											setSelectedReview={setSelectedReview}
-											setRemoveReviewOpen={setRemoveReviewOpen}
-											setEditReviewOpen={setEditReviewOpen}
-											setPage={setPage}
-											hasMore={hasMore}
-											isLoading={isLoading}
-											setIsLoading={setIsLoading}
+									</div>
+								</Tab>
+							</TabList>
+							<TabPanels>
+								<TabPanel>
+									<div className='flex lg:flex-row lg:gap-2 lg:divide-x lg:divide-gray-200'>
+										<MobileReviewFilters
+											mobileFiltersOpen={mobileFiltersOpen}
+											setMobileFiltersOpen={setMobileFiltersOpen}
+											countryFilter={countryFilter}
+											stateFilter={stateFilter}
+											cityFilter={cityFilter}
+											zipFilter={zipFilter}
+											dynamicCityOptions={dynamicCityOptions}
+											dynamicStateOptions={dynamicStateOptions}
+											zipOptions={dynamicZipOptions}
+											dynamicZipOptions={dynamicZipOptions}
+											updateParams={updateParams}
+											dispatch={dispatch}
+											fetchDynamicFilterOptions={fetchDynamicFilterOptions}
+											query={query}
 										/>
-									)}
-								</div>
-							</TabPanel>
-							<TabPanel>
-								<MapComponent />
-								<p className='mt-6 text-xs leading-7 text-gray-600'>
-									This map component is currently in Beta. It is provided “as
-									is” and may contain bugs, inaccuracies, or incomplete
-									features. We are actively working to improve the functionality
-									and accuracy of this component. <br />
-									Please note: The map data may not be fully accurate or
-									up-to-date. Some features may not work as expected.
-									Performance may vary depending on your device and network
-									conditions.
-								</p>
-							</TabPanel>
-						</TabPanels>
-					</TabGroup>
+										<ReviewFilters
+											selectedSort={selectedSort}
+											setSelectedSort={setSelectedSort}
+											sortOptions={sortOptions}
+											countryFilter={countryFilter}
+											stateFilter={stateFilter}
+											cityFilter={cityFilter}
+											zipFilter={zipFilter}
+											dynamicCityOptions={dynamicCityOptions}
+											dynamicStateOptions={dynamicStateOptions}
+											zipOptions={dynamicZipOptions}
+											dynamicZipOptions={dynamicZipOptions}
+											updateParams={updateParams}
+											loading={isLoading}
+											dispatch={dispatch}
+											fetchDynamicFilterOptions={fetchDynamicFilterOptions}
+											query={query}
+										/>
+										{!reviews.length ? (
+											<div className='mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center p-6'>
+												<h1 className='mt-4 text-3xl   text-gray-900 sm:text-5xl'>
+													No results found
+												</h1>
+												<p className='mt-6 text-base leading-7 text-gray-600'>
+													Sorry, we couldn't find any results for those filters.
+												</p>
+											</div>
+										) : (
+											<InfiniteScroll
+												data={reviews}
+												setReportOpen={setReportOpen}
+												setSelectedReview={setSelectedReview}
+												setRemoveReviewOpen={setRemoveReviewOpen}
+												setEditReviewOpen={setEditReviewOpen}
+												setPage={setPage}
+												hasMore={hasMore}
+												isLoading={isLoading}
+												setIsLoading={setIsLoading}
+											/>
+										)}
+									</div>
+								</TabPanel>
+								<TabPanel>
+									<MapComponent />
+									<p className='mt-6 text-xs leading-7 text-gray-600'>
+										This map component is currently in Beta. It is provided “as
+										is” and may contain bugs, inaccuracies, or incomplete
+										features. We are actively working to improve the
+										functionality and accuracy of this component. <br />
+										Please note: The map data may not be fully accurate or
+										up-to-date. Some features may not work as expected.
+										Performance may vary depending on your device and network
+										conditions.
+									</p>
+								</TabPanel>
+							</TabPanels>
+						</TabGroup>
+					</div>
 				</div>
-			</div>
 			)}
 		</>
 	)
