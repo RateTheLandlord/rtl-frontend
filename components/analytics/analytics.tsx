633d3124540592428f0ec6614db96f3ff62f5340
stephenmartin2951 <stephenmartin2951@pm.me>
2024-12-18 17:02:53 -0600

    lint

--- components/analytics/analytics.tsx
+++ components/analytics/analytics.tsx
@@ -7,7 +7,6 @@ import useSWR from 'swr'
 import { fetchWithBody } from '@/util/helpers/fetcher'
 import { AnalyticsResponse } from '@/lib/analytics/models/review'
 
-
 interface AnalyticProps {
 	countryFilter: Options | null
 	stateFilter: Options | null
@@ -16,13 +15,12 @@ interface AnalyticProps {
 	landlordFilter: string | undefined
 }
 
-const AnalyticsComponent = ({ 
+const AnalyticsComponent = ({
 	countryFilter,
 	stateFilter,
 	cityFilter,
 	zipFilter,
-	landlordFilter 
-
+	landlordFilter,
 }: AnalyticProps) => {
 	const { t } = useTranslation('reviews')
 	// Consolidate related states
@@ -31,14 +29,16 @@ const AnalyticsComponent = ({
 		city: cityFilter as Options | null,
 		country: countryFilter as Options | null,
 		state: stateFilter as Options | null,
-		landlord: landlordFilter as string | null
+		landlord: landlordFilter as string | null,
 	})
 
 	const { data, error } = useSWR<AnalyticsResponse, Error>(
-		['/api/review/review-analytics', { landlordFilter, stateFilter, countryFilter, cityFilter, zipFilter }],
+		[
+			'/api/review/review-analytics',
+			{ landlordFilter, stateFilter, countryFilter, cityFilter, zipFilter },
+		],
 		fetchWithBody,
 	)
-
 
 	const fetchDynamicFilterOptions = async () => {
 		try {
@@ -63,61 +63,89 @@ const AnalyticsComponent = ({
 
 	return (
 		<div className='grid w-full grid-cols-2'>
-			<div className='flex-1'>
-				left column
-			</div>
+			<div className='flex-1'>left column</div>
 			<div className='flex-1'>
 				<div className='h-4'></div>
 				<div className='border p-4'>
-					<div className='flex items-center justify-center text-xl pb-12 pt-2 underline bold'>Total Reviews</div>
+					<div className='bold flex items-center justify-center pb-12 pt-2 text-xl underline'>
+						Total Reviews
+					</div>
 					<div className='grid w-full grid-cols-3 border-b'>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 90 Days:</div>
-							<div>{data.totalReviewsT90.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+							<div>
+								{data.totalReviewsT90
+									.toString()
+									.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+							</div>
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 180 Days:</div>
-							{data.totalReviewsT180.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT180
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 360 Days:</div>
-							{data.totalReviewsT360.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT360
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
 					</div>
 				</div>
 				<div className='h-4'></div>
 				<div className='border p-4'>
-					<div className='flex items-center justify-center text-xl pb-12 pt-2 underline bold'>Average Rating</div>
+					<div className='bold flex items-center justify-center pb-12 pt-2 text-xl underline'>
+						Average Rating
+					</div>
 					<div className='grid w-full grid-cols-3 border-b'>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 90 Days:</div>
-							<div>{data.totalReviewsT90.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+							<div>
+								{data.totalReviewsT90
+									.toString()
+									.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+							</div>
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 180 Days:</div>
-							{data.totalReviewsT180.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT180
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 360 Days:</div>
-							{data.totalReviewsT360.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT360
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
 					</div>
 				</div>
 				<div className='h-4'></div>
 				<div className='border p-4'>
-					<div className='flex items-center justify-center text-xl pb-12 pt-2 underline bold'>Median Reported Rent</div>
+					<div className='bold flex items-center justify-center pb-12 pt-2 text-xl underline'>
+						Median Reported Rent
+					</div>
 					<div className='grid w-full grid-cols-3 border-b'>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 90 Days:</div>
-							<div>{data.totalReviewsT90.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+							<div>
+								{data.totalReviewsT90
+									.toString()
+									.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+							</div>
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 180 Days:</div>
-							{data.totalReviewsT180.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT180
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
-						<div className='flex flex-col items-center justify-center text-lg border'>
+						<div className='flex flex-col items-center justify-center border text-lg'>
 							<div>Last 360 Days:</div>
-							{data.totalReviewsT360.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+							{data.totalReviewsT360
+								.toString()
+								.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
 						</div>
 					</div>
 				</div>
