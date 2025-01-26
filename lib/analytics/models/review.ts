9a46061a6519a1bf65410495e7360821e76ef108
kellenwiltshire <kellenwiltshire@outlook.com>
2025-01-26 16:38:49 -0500

    update api calls to reflect data used in analytics component

--- lib/analytics/models/review.ts
+++ lib/analytics/models/review.ts
@@ -1,20 +1,15 @@
 import { AnalyticsResponseInterface } from '@/util/interfaces/interfaces'
 
 export type AnalyticsResponse = {
-	totalReviewsT90: number,
-	totalReviewsT180: number,
-	totalReviewsT365: number,
-	avgRatingT90: number,
-	avgRatingT180: number,
-	avgRatingT365: number,
-	medianRentT90: number,
-	medianRentT180: number,
+	avgRatingT90: number
+	avgRatingT180: number
+	avgRatingT365: number
+	medianRentT90: number
+	medianRentT180: number
 	medianRentT365: number
 }
 
 export type AnalyticsChartResponse = {
-	reviewsChartData: AnalyticsResponseInterface[],
-	avgRatingChartData: AnalyticsResponseInterface[],
+	avgRatingChartData: AnalyticsResponseInterface[]
 	medianChartData: AnalyticsResponseInterface[]
 }
-
