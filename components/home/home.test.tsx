02d5d0efd3b96fbdd51f7e89f264f9d39e092c84
kellenwiltshire <kellenwiltshire@outlook.com>
2025-02-02 11:51:18 -0500

    Add home tests

--- components/home/home.test.tsx
+++ components/home/home.test.tsx
@@ -4,6 +4,8 @@
 import { render, screen } from '@testing-library/react'
 import Hero from '@/components/home/hero'
 import IconSection from '@/components/home/icon-section'
+import { axe, toHaveNoViolations } from 'jest-axe'
+expect.extend(toHaveNoViolations)
 
 describe('Homepage', () => {
 	test('Hero component renders', () => {
@@ -13,5 +15,10 @@ describe('Homepage', () => {
 	test('Icon section component renders', () => {
 		render(<IconSection />)
 		expect(screen.getByTestId('home-icon-section-1')).toBeInTheDocument()
+	})
+	it('Should not have a11y violation', async () => {
+		const { container } = render(<Hero />)
+		const result = await axe(container)
+		expect(result).toHaveNoViolations()
 	})
 })
