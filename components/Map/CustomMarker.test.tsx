13bd8853319e28434c2398cd1a7b87cc5892b225
kellenwiltshire <kellenwiltshire@outlook.com>
2025-02-02 13:43:02 -0500

    Linting

--- components/Map/CustomMarker.test.tsx
+++ components/Map/CustomMarker.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import CustomMarker from './CustomMarker'
 import { IZipLocations } from '@/lib/location/location'
