2a74014b438d677b3993d0bef4da098655574b18
stephenmartin2951 <stephenmartin2951@pm.me>
2025-02-05 16:03:50 -0600

    lint

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,12 +1,12 @@
 // pages/api/cron.ts
-import cron from 'node-cron';
+import cron from 'node-cron'
 
 // Schedule a cron job to run every minute
 cron.schedule('* * * * *', () => {
-  console.log('Cron job is running every minute!');
-  // Add your task here
-});
+	console.log('Cron job is running every minute!')
+	// Add your task here
+})
 
 export default function handler(req, res) {
-  res.status(200).json({ message: 'Cron job API is running!' });
+	res.status(200).json({ message: 'Cron job API is running!' })
 }
