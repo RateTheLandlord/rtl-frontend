3b24442f109f81ac901b2b77907492ce2329e900
kellenwiltshire <kellenwiltshire@outlook.com>
2025-02-07 18:24:30 -0500

    Update import

--- check-tests.sh
+++ check-tests.sh
@@ -5,6 +5,13 @@ STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -E '\.tsx$
 
 # Loop through each staged file and check for test files and a11y tests
 for file in $STAGED_FILES; do
+  echo "Processing file: $file"  # Debug statement
+
+  if [[ "$file" == *"/pages/"* ]]; then
+    echo "Skipping file in pages directory: $file"  # Debug statement
+    continue
+  fi
+  
   if [[ "$file" != *".test.tsx" ]]; then
     test_file="${file%.tsx}.test.tsx"
     if [ ! -f "$test_file" ]; then
