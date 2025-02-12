efd6834c1dde6736e2faf83171b83a9df84fd25c
kellenwiltshire <kellenwiltshire@outlook.com>
2025-02-12 08:05:02 -0500

    Working on Knip Integration

--- pages/login.tsx
+++ pages/login.tsx
@@ -35,7 +35,7 @@ function Login(): JSX.Element {
 						<div className='mt-10 flex items-center justify-center gap-x-6'>
 							<a
 								href={`/login`}
-								className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+								className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
 							>
 								Login
 							</a>
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,7 @@
-module.exports = {
-	plugins: [require('prettier-plugin-tailwindcss')],
-	tailwindConfig: './styles/tailwind.config.js',
-}
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {}
+
+export default config
