7229614d5646850429a6c938742f913ea639ad34
kellenwiltshire <kellenwiltshire@outlook.com>
2025-02-24 16:19:42 -0500

    cache update

--- components/create-review/components/LandlordComboBox.tsx
+++ components/create-review/components/LandlordComboBox.tsx
@@ -52,7 +52,7 @@ export default function LandlordComboBox({
 					leaveFrom='opacity-100'
 					leaveTo='opacity-0'
 				>
-					<ComboboxOptions className='ring-opacity-5 absolute z-10 mt-1 flex max-h-60 w-60 flex-col overflow-auto rounded-md bg-white text-base ring-1 shadow-lg ring-black focus:outline-none sm:text-sm'>
+					<ComboboxOptions className='ring-opacity-5 absolute z-10 mt-1 flex max-h-60 w-60 flex-col overflow-auto rounded-md bg-white text-base shadow-lg ring-1 ring-black focus:outline-none sm:text-sm'>
 						{suggestions.length === 0 && state !== '' ? (
 							isSearching ? (
 								<div className='relative cursor-default px-4 py-2 text-gray-700 select-none'>
--- components/reviews/ui/locationCombobox.tsx
+++ components/reviews/ui/locationCombobox.tsx
@@ -66,7 +66,7 @@ export default function ComboBox({
 					leaveFrom='opacity-100'
 					leaveTo='opacity-0'
 				>
-					<ComboboxOptions className='sm:text-md ring-opacity-5 absolute z-10 mt-1 max-h-60 w-9/12 overflow-auto rounded-md bg-white py-1 text-base ring-1 shadow-lg ring-black focus:outline-none sm:w-1/2 md:w-8/12 lg:w-9/12 xl:w-7/12 2xl:w-7/12'>
+					<ComboboxOptions className='sm:text-md ring-opacity-5 absolute z-10 mt-1 max-h-60 w-9/12 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black focus:outline-none sm:w-1/2 md:w-8/12 lg:w-9/12 xl:w-7/12 2xl:w-7/12'>
 						{filterOptions.length === 0 && query !== '' ? (
 							<div className='relative cursor-default px-4 py-2 text-gray-700 select-none'>
 								Nothing found.
