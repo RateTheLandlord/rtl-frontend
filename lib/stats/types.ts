8010b35b668cad0c7824db0408f7505c1b09ade5
kellenwiltshire <kellenwiltshire@outlook.com>
2025-03-01 11:31:51 -0500

    Properly Type stats.ts File Fixes #252

--- lib/stats/types.ts
+++ lib/stats/types.ts
@@ -1,21 +1,9 @@
-export interface StatsQuery {
-	startDate?: string
-	groupBy?: string
-}
-
-export interface DetailedStats {
-	date: string
-	country_codes: Record<string, number>
-	cities: Record<string, number>
-	state: Record<string, number>
-	zip: Record<string, number>
-	total?: number
-}
+export type CountryStats = Record<
+	string,
+	{ total: number; states: { key: string; total: number }[] }
+>
 
 export interface TotalStats {
 	total_reviews: number
-	countryStats: Record<
-		string,
-		{ total: number; states: { key: string; total: number }[] }
-	>
+	countryStats: CountryStats
 }
