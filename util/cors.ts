e2b528cca77095882abc431d0f86ff9287c2591c
kellenwiltshire <kellenwiltshire@outlook.com>
2025-03-01 11:47:48 -0500

    Resolve no-unsafe-call

--- util/cors.ts
+++ util/cors.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Cors from 'cors'
 import { NextApiRequest, NextApiResponse } from 'next'
 
--- util/helpers/toTitleCase.ts
+++ util/helpers/toTitleCase.ts
@@ -1,4 +1,4 @@
-export const toTitleCase = (str) => {
+export const toTitleCase = (str: string) => {
 	return str.replace(/\w\S*/g, function (txt) {
 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
 	})
