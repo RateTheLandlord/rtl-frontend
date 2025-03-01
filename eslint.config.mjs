d6df79cf80698d8191a6d28bf1abcab29acf71e9
kellenwiltshire <kellenwiltshire@outlook.com>
2025-03-01 11:50:45 -0500

    Resolve no-unsafe-member-access

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,7 +48,6 @@ export default tseslint.config({
 	},
 	// TODO RULES TO FIX - SET TO WARN FOR NOW
 	rules: {
-		'@typescript-eslint/no-unsafe-member-access': 'warn',
 		'@typescript-eslint/no-floating-promises': 'warn',
 		'@typescript-eslint/no-unsafe-argument': 'warn',
 		'@typescript-eslint/require-await': 'warn',
