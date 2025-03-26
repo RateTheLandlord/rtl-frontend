f24fd80949185be50d83ee61d5c6673f28ce8ef1
kellenwiltshire <kellenwiltshire@outlook.com>
2025-03-26 18:22:48 -0400

    Remove auth

--- pages/api/force-revalidate.tsx
+++ pages/api/force-revalidate.tsx
@@ -1,21 +1,14 @@
 import { runMiddleware } from '@/util/cors'
 import rateLimitMiddleware from '@/util/rateLimit'
-import { getSession } from '@auth0/nextjs-auth0'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	await runMiddleware(req, res)
-	const session = await getSession(req, res)
-	const user = session?.user
 
 	const path = req.query.path as string
 
 	if (!path) {
 		return res.status(401).json({ message: 'Invalid path' })
-	}
-
-	if (!user || user.role !== 'ADMIN') {
-		return res.status(401).json({ message: 'Invalid token' })
 	}
 
 	try {
