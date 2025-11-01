927823d6e0b6822067603b670307ef485d12a25a
stephenmartin2951 <stephenmartin2951@pm.me>
2025-11-01 17:07:32 -0500

    db changes

---
+++ migrations/014-review.ts
@@ -0,0 +1,10 @@
+exports.up = async function (DB) {
+	await DB`
+    ALTER TABLE review
+    ADD COLUMN user_code TEXT DEFAULT NULL,
+    ADD COLUMN last_user_attempt TIMETZ DEFAULT NULL,
+    ADD COLUMN number_user_attempts numeric DEFAULT 0,
+    ADD COLUMN number_user_edits numeric DEFAULT 0,
+    ADD COLUMN has_user_code BOOLEAN DEFAULT false,
+    `
+}
