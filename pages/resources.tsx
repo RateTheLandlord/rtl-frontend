1929fab2a9d5ef25ad97c6ef61c4852eee7665f8
Rate The Landlord <107703532+RateTheLandlordAdmin@users.noreply.github.com>
2026-08-27 19:10:25 -0400

    Release 2.40 (#371)
    * dockerhub-conversion-test (#369)
    
    * fix-docker-build (#370)
    
    * Attempt again to fix docker build
    
    * Tweak build process
    
    * Tweak rendering
    
    ---------
    
    Co-authored-by: Kellen Wiltshire <Kellenwiltshire@outlook.com>

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -6,6 +6,8 @@ import ResourceList from '@/components/resources/ResourceList'
 import { ResourceResponse } from '@/util/interfaces/interfaces'
 import { getResources } from '@/lib/tenant-resource/resource'
 import { useTranslations } from 'next-intl'
+import { readLocaleFile } from '@/util/readLocalFile'
+import Spinner from '@/components/ui/Spinner'
 
 interface IProps {
 	data: ResourceResponse
@@ -22,6 +24,10 @@ function Resources({ data }: IProps): JSX.Element {
 	const twitterHandle = '@r8thelandlord'
 	const siteName = 'RateTheLandlord.org'
 	const t = useTranslations('resources')
+
+	if (!data?.resources) return <Spinner />
+
+	if (data?.resources.length === 0) return <div>Error Loading Resources</div>
 	return (
 		<div className='flex w-full justify-center'>
 			<NextSeo
@@ -62,48 +68,33 @@ function Resources({ data }: IProps): JSX.Element {
 
 export default Resources
 
-//Page is statically generated at build time and then revalidated at a minimum of every 30 minutes based on when the page is accessed
-export async function getStaticProps({ locale }: { locale: string }) {
-	const resourcesMessages = (await import(
-		`@/messages/${locale}/resources.json`
-	)) as Record<string, string>
-	const alertsMessages = (await import(
-		`@/messages/${locale}/alerts.json`
-	)) as Record<string, string>
-	const layoutMessages = (await import(
-		`@/messages/${locale}/layout.json`
-	)) as Record<string, string>
-	const filtersMessages = (await import(
-		`@/messages/${locale}/filters.json`
-	)) as Record<string, string>
+export async function getServerSideProps({ locale }: { locale: string }) {
+	const resourcesMessages = readLocaleFile('resources', locale)
+	const alertsMessages = readLocaleFile('alerts', locale)
+	const layoutMessages = readLocaleFile('layout', locale)
+	const filtersMessages = readLocaleFile('filters', locale)
+
 	const data = await getResources({})
-	if (data) {
+
+	if (!data || data.resources.length === 0) {
 		return {
-			props: JSON.parse(
-				JSON.stringify({
-					data: data,
-					messages: {
-						...resourcesMessages,
-						...alertsMessages,
-						...layoutMessages,
-						...filtersMessages,
-					},
-				}),
-			),
-			revalidate: 100,
-		}
-	} else {
-		return {
-			props: {
-				messages: {
-					...resourcesMessages,
-					...alertsMessages,
-					...layoutMessages,
-					...filtersMessages,
-				},
-				data: [],
+			redirect: {
+				permanent: false,
+				destination: '/404',
 			},
-			revalidate: 100,
 		}
+	}
+
+	return {
+		props: {
+			data: JSON.parse(JSON.stringify(data)),
+			messages: {
+				...resourcesMessages,
+				...alertsMessages,
+				...layoutMessages,
+				...filtersMessages,
+			},
+		},
+		revalidate: 100,
 	}
 }
